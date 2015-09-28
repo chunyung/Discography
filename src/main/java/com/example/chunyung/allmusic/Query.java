b1526243db3d9baa9b72e46f3770bb92e1a74aca
chunyung <chunyung@gmail.com>
2015-09-28 16:16:39 -0700

    Update Query.java

--- src/main/java/com/example/chunyung/allmusic/Query.java
+++ src/main/java/com/example/chunyung/allmusic/Query.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Query {
+public class Query {// used to form query object and Jackson JSON api to form JSON request and response between client and server
     public List<String> keywords;
     public Query() {
         this.keywords = new ArrayList<String>();
